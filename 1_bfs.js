efb7dd007d47d67f281100336dd77b4d902c6db1
Cesar Aguilar <Blueavian9@gmail.com>
2023-07-12 10:13:43 -0700

    Initial commit

--- 1_bfs.js
+++ 1_bfs.js
@@ -32,3 +32,13 @@ head.left = new node(3)
 head.right = new node(9)
 
 console.log(traverse(head)) // 6, 3, 9
+
+/*
+1. we need an empty output array.
+2. Initialize a queue
+3. then apply queue.push(head)
+4. while (queue has data) -
+5. serve the current node (shift - adding to output array + add children if any to the queue.
+6. keep repeating until the queue is empty.
+10. return output array.
+*/
---
+++ dfs-Class.js
@@ -0,0 +1,48 @@
+class node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+function traverse(root) {
+  const output = [];
+  const stack = [];
+  const current = root;
+
+  while (stack.length) {
+    stack.push(current);
+    current = current.left;
+  }
+}
+
+// Test cases:
+const head = new node(6);
+head.left = new node(3);
+head.right = new node(9);
+
+console.log(traverse(head)); // 3, 6, 9
+/* 
+       6 
+      / \
+     3   9
+   
+     Last In First Out === L.I.F.O.
+  */
+// Implement an in-order depth-first traversal for a binary tree using the provided class for a binary tree node.
+// the return should be a list of values in the binary tree, based on an in-order (left, current, right) depth-first traversal
+
+// Hint: you can implement DFS either with recursion (using the system call stack) or by manually declaring your own stack data structure.
+// For a quick and easy stack implementation:
+// const stack = []
+// then stack.push(value) to push and stack.pop() to pop
+
+/*
+  1. Initialize Output array and stack as an empty array.
+  2. keep repeating as long as stack has values.
+  3. get the current value using pop().
+  4. add/push root to stack.
+  5. check for left/right children and serve them accordingly.
+  6. return the output array.
+  */
