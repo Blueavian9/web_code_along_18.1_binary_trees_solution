29b51500081be9500dbaa9967c6664e7d4d2c830
Cesar Aguilar <Blueavian9@gmail.com>
2023-10-25 22:59:57 -0700

    Initial commit

--- 1_bfs.js
+++ 1_bfs.js
@@ -1,44 +1,80 @@
 class node {
   constructor(value) {
-    this.value = value
-    this.left = null
-    this.right = null
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
 function traverse(root) {
-  if (root === null) return
-  const values = []
-  const queue = []
-  queue.push(root)
+  if (root === null) return;
+  const values = [];
+  const queue = [];
+  queue.push(root);
 
   while (queue.length) {
-    const current = queue.shift()
-    values.push(current.value)
+    const current = queue.shift();
 
-    if (current.left !== null) {
-      queue.push(current.left)
-    }
-    if (current.right !== null) {
-      queue.push(current.right)
-    }
+    values.push(current.value);
   }
-  return values
+  if (current.left !== null) {
+    queue.push(current.left);
+  }
+  if (current.right !== null) {
+    queue.push(current.right);
+  }
 }
+return values;
 
 // Test cases:
-const head = new node(6)
-head.left = new node(3)
-head.right = new node(9)
+const head = new node(6);
+head.left = new node(3);
+head.right = new node(9);
 
-console.log(traverse(head)) // 6, 3, 9
+console.log(traverse(head)); // 6, 3, 9
 
-/*
-1. we need an empty output array.
-2. Initialize a queue
-3. then apply queue.push(head)
-4. while (queue has data) -
-5. serve the current node (shift - adding to output array + add children if any to the queue.
-6. keep repeating until the queue is empty.
-10. return output array.
-*/
+
+// class node {
+//   constructor(value) {
+//     this.value = value
+//     this.left = null
+//     this.right = null
+//   }
+// }
+
+// function traverse(root) {
+//   if (root === null) return
+//   const values = []
+//   const queue = []
+//   queue.push(root)
+
+//   while (queue.length) {
+//     const current = queue.shift();
+//     values.push(current.value)
+
+//     if (current.left !== null) {
+//       queue.push(current.left)
+//     }
+//     if (current.right !== null) {
+//       queue.push(current.right)
+//     }
+//   }
+//   return values
+// }
+
+// // Test cases:
+// const head = new node(6)
+// head.left = new node(3)
+// head.right = new node(9)
+
+// console.log(traverse(head)) // 6, 3, 9
+
+// /*
+// 1. we need an empty output array.
+// 2. Initialize a queue
+// 3. then apply queue.push(head)
+// 4. while (queue has data) -
+// 5. serve the current node (shift - adding to output array + add children if any to the queue.
+// 6. keep repeating until the queue is empty.
+// 10. return output array.
+// */
