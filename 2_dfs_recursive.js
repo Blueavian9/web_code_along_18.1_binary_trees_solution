501bc9080216dab90ba57ccc85044ba113b25136
Cesar Aguilar <Blueavian9@gmail.com>
2023-11-01 15:07:25 -0700

    Initial update

--- 2_dfs_recursive.js
+++ 2_dfs_recursive.js
@@ -6,19 +6,14 @@ class node {
   }
 }
 
-function traverse(root) {
-  const values = [];
-  traverse(root, values);
-  return values;
-}
 
-function traverse(current, values) {
+function traverse_node(current, values) {
   if (current.left !== null) {
-    traverse(current.left, values);
+    traverse_node(current.left, values);
   }
   values.push(current.value);
   if (current.right !== null) {
-    traverse(current.right, values);
+    traverse_node(current.right, values);
   }
 }
 
@@ -28,38 +23,5 @@ head.left = new node(3);
 head.right = new node(9);
 
 console.log(traverse(head)); // 3, 6, 9
-
-// This is the recursive solution. Could also be implemented with an explicitly-declared stack data structure.
-
-// class node {
-//   constructor(value) {
-//     this.value = value
-//     this.left = null
-//     this.right = null
-//   }
-// }
-
-// function traverse(root) {
-//   const values = []
-//   traverse_node(root, values)
-//   return values
-// }
-
-// function traverse_node(current, values) {
-//   if (current.left !== null) {
-//     traverse_node(current.left, values)
-//   }
-//   values.push(current.value)
-//   if (current.right !== null) {
-//     traverse_node(current.right, values)
-//   }
-// }
-
-// // Test cases:
-// const head = new node(6)
-// head.left = new node(3)
-// head.right = new node(9)
-
-// console.log(traverse(head)) // 3, 6, 9
 
 // This is the recursive solution. Could also be implemented with an explicitly-declared stack data structure.
